677cf4cd57d3b4ce2096e2f80ea2146eb07c34a9
Tim Mundt <tim.mundt26@gmail.com>
2025-07-02 19:58:36 +0200

    Delete unused customer_routes

--- blueprints/customer_routes.py
+++ blueprints/customer_routes.py
@@ -87,8 +87,6 @@ def ticket_confirmation():
         flash("Ticket-Daten nicht gefunden.")
         return redirect(url_for('customer.ticket_step1'))
     
-    
-    
     if request.method == 'POST':
         try:
             ticket = Ticket(
@@ -137,44 +135,6 @@ def get_account_info():
     return render_template("customer_account.html")
 
 
-#Nicht Getestet, rendertemplate änder
-@customer_r.route('/repairers', methods=['GET'])
-@login_required
-def get_repairers():
-    repaires=db.session.execute(
-        db.select(User).
-        filter_by(role='repairer').order_by(User.last_name)).scalars()
-    
-    return render_template('customer_account-html', repaires=repaires)
-
-
-#Nicht Getestet, rendertemplate änder
-@customer_r.route('/filter-by-repairers', methods=['GET'])
-@login_required
-def filter_by_repairers():
-    model_series=request.args.get('skill')
-    repaires=db.session.execute(
-        db.select(User).
-        join(User.repairer).
-        join(Repairer.skills_rl).
-        where(model_series=model_series)).scalars()
-    
-    return render_template('customer_account-html', repaires=repaires)
-
-
-#Nicht Getestet
-@customer_r.route('/create-ticket', methods=['POST'])
-@login_required
-def create_ticket():
-    repairer_id=request.args.get('repairer_id')
-    model_series=request.args('model_series')
-    ticket=Ticket(customer_id=current_user.user_id, repairer_id=repairer_id, model=model_series)
-    db.session.add(ticket)
-    db.session.commit()
-
-    return redirect(url_for('customer.filter_by_repaires'))
-    
-
 
 @customer_r.route('/get-tickets')
 @login_required
@@ -192,8 +152,6 @@ def get_tickets():
     return render_template('get_tickets.html', tickets=tickets, status=status)
 
 
-
-#Nicht Getestet
 @customer_r.route('/delete-ticket/<int:ticket_id>', methods=['POST'])
 @login_required
 def delete_ticket(ticket_id):
@@ -204,7 +162,7 @@ def delete_ticket(ticket_id):
     return redirect (url_for('customer.get_tickets'))
     
 
-#Nicht Getestet, render template ändern
+
 @customer_r.route('/open-chat/<int:ticket_id>', methods=['GET'])
 @login_required
 def open_chat(ticket_id):
