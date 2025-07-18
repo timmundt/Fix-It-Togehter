2552286dafbad5e9c0e26805cb7b0af7c3e50553
Baran Celik <baran99celik@gmail.com>
2025-07-18 18:02:43 +0200

    Add send_message route

--- blueprints/chat_routes.py
+++ blueprints/chat_routes.py
@@ -1,6 +1,7 @@
 from flask import Flask, Blueprint, flash, render_template, redirect, request, url_for
 from flask_login import login_required, current_user
 from database import db, ChatMessage, Ticket
+from datetime import datetime
 
 chat_r =Blueprint('chat', __name__)
 
@@ -25,3 +26,40 @@ def open_chat(ticket_id):
     ).scalars().all()
 
     return render_template('chat.html', ticket=ticket, chat_messages=chat_messages)
+
+@chat_r.route('/chat/send', methods=['POST'])
+@login_required
+def send_message():
+  ticket_id = request.form.get('ticket_id')
+  content = request.form.get('content')
+
+  if not ticket_id or not content:
+    flash("Nachricht konnte nicht gesendet werden")
+    return redirect(url_for('chat.open_chat', ticket_id=ticket_id))
+   
+  ticket = db.session.execute(
+    db.select(Ticket).filter_by(ticket_id=ticket_id)
+  ).scalar_one_or_none()
+
+  if not ticket:
+    flash("Ticket nicht gefunden.")
+    return redirect(url_for('chat.open_chat', ticket_id=ticket_id))
+   
+  if current_user.repairer:
+    sender_role = "repairer"
+  elif current_user.customer:
+     sender_role = "customer"
+  else:
+     flash("Unbekannte Benutzerrolle.")
+     return redirect(url_for('auth.login'))
+  
+  message = ChatMessage(
+     ticket_id = ticket_id,
+     message = content,
+     timestamp = datetime.now(),
+     sender_role = sender_role
+  )
+  db.session.add(message)
+  db.session.commit()
+
+  return redirect(url_for('chat.open_chat', ticket_id=ticket_id))
